3578e3b204a1d0ad62ec02ce63d8e455952866fc
zindha <zindhak@gmail.com>
2026-07-25 20:46:30 +0000

    Fixed issues in build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.emicalculator.emi_calculator"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
